55501e54031d32a21357bf9d71b38c8023ad32d7
vbystroem <veronica.bystroem@gmail.com>
2026-02-11 15:25:36 +0100

    Imported logo and started work on color system

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -4,7 +4,7 @@ export default function Navigation() {
   return (
     <nav className={styles.header}>
       <a className={styles.navlink} href="">
-        PokéScrandle
+        <img src="/src/assets/vtmnlogo_light_landscape.svg" alt="Votémon: the daily voting game" className={styles.logo}/>
       </a>
       <a className={styles.navlink} href="">
         Leaderboards
