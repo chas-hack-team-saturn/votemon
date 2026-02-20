387456ebb6576d70fb663bdbad64cff6b7f83c80
vbystroem <veronica.bystroem@gmail.com>
2026-02-20 22:27:27 +0100

    Configured light / dark mode and more css variables

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,14 +1,16 @@
 import styles from "./Navigation.module.css";
+import ThemeToggle from "./ThemeToggle";
 
 export default function Navigation() {
   return (
     <nav className={styles.header}>
       <a className={styles.navlink} href="">
-        <img src="/src/assets/vtmnlogo_light_landscape.svg" alt="Votémon: the daily voting game" className={styles.logo}/>
+        <img src="/src/assets/vtmnlogo_light_landscape.svg" alt="Votémon: the daily voting game" className={styles.logo} />
       </a>
       <a className={styles.navlink} href="">
         Leaderboards
       </a>
+      <ThemeToggle />
     </nav>
   );
 }
