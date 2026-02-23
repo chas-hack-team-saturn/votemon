8c070861e4db2056d7d86326481910b93a51d9e7
Alex <alexcoh19@outlook.com>
2026-02-23 11:59:09 +0100

    In BackendAPI/Services/ folder: Added GenerateScript.cs for generating MySQL scripts. In docker/ folder: Added and updated insert.sql and update.sql

---
+++ BackendAPI/Services/GenerateScript.cs
@@ -0,0 +1,57 @@
+﻿namespace BackendAPI.Services
+{
+	/// <summary>
+	///     Utility class for generating MySQL database scripts.
+	/// </summary>
+	/// <remarks>
+	///		Use these scripts when <see cref="Endpoints"/> methods are not working.
+	/// </remarks>
+	static public class GenerateScript
+	{
+		/// <summary>
+		///		Generates an insert.sql script that inserts all pokemons into Pokemon table.
+		/// </summary>
+		/// <remarks>
+		///		<para>Will overwrite existing insert.sql script.</para>
+		///		Requires <see cref="PokeApiMethods.PokemonNames"/> to be filled before invoking this method.
+		/// </remarks>
+		static internal void InsertIntoPokemon()
+		{
+			using (var writer = new StreamWriter("insert.sql", false))
+			{
+				writer.WriteLine("USE PokeScrandle");
+				writer.WriteLine("INSERT INTO Pokemon (Name) VALUES");
+
+				for (int i = 0; i < PokeApiMethods.PokemonNames.Count; i++)
+				{
+					if (i == PokeApiMethods.PokemonNames.Count - 1)
+						writer.WriteLine($"    ('{PokeApiMethods.PokemonNames[i]}');");
+					else
+						writer.WriteLine($"    ('{PokeApiMethods.PokemonNames[i]}'),");
+				}
+			}
+		}
+
+        /// <summary>
+        ///		Generates update.sql script that updates each row's Name column in Pokemon table.
+        /// </summary>
+        /// <remarks>
+		///		<para>Will overwrite existing update.sql script.</para>
+        ///		Requires <see cref="PokeApiMethods.PokemonNames"/> to be filled before invoking this method.
+        /// </remarks>
+        static internal void UpdateAllPokemonNames()
+		{
+			using (var writer = new StreamWriter("update.sql", false))
+			{
+				writer.WriteLine("USE PokeScrandle");
+
+				for (int i = 0; i < PokeApiMethods.PokemonNames.Count; i++)
+				{
+					writer.WriteLine(
+						$"UPDATE Pokemon SET Name = '{PokeApiMethods.PokemonNames[i]}' WHERE DexId = {i + 1};"
+						);
+				}
+			}
+		}
+	}
+}
