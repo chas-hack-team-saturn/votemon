8929c1a54c09310358573b708967dc271d870d71
sheymaisse-chas <sheyma.isse@chasacademy.se>
2026-02-23 16:37:12 +0100

    added a comment developent of leaderboard

--- frontend/src/pages/Leaderboard.tsx
+++ frontend/src/pages/Leaderboard.tsx
@@ -1,16 +1,18 @@
 import { NavLink } from "react-router";
 
 const Leaderboard = () => {
+  // lägg en end-point på 100
 
-
-    return (
-        <div>
-            <ul className="leaderboard-list">
-                <li className="User-1">Emil56</li>
-            </ul>
-            <button><NavLink to="/">Kör igen</NavLink></button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <ul className="leaderboard-list">
+        <li className="User-1">Pikachu</li>
+      </ul>
+      <button>
+        <NavLink to="/">Kör igen</NavLink>
+      </button>
+    </div>
+  );
+};
 
 export default Leaderboard;
